7d046bd72875b7ba7f2aee1cb5dbbc26eafdc8f5
josephrebne <joey@chargeover.com>
2025-02-05 17:45:56 -0600

    finished

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,69 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "@/views/EventList.vue";
-import EventLayout from "@/views/event/Layout.vue";
-import EventDetails from "@/views/event/Details.vue";
-import EventRegister from "@/views/event/Register.vue";
-import EventEdit from "@/views/event/Edit.vue";
-import About from "@/views/About.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '@/views/EventList.vue'
+import EventLayout from '@/views/event/Layout.vue'
+import EventDetails from '@/views/event/Details.vue'
+import EventRegister from '@/views/event/Register.vue'
+import EventEdit from '@/views/event/Edit.vue'
+import About from '@/views/About.vue'
 
 const routes = [
-  {
-    path: "/",
-    name: "EventList",
-    component: EventList,
-    props: (route) => ({ page: parseInt(route.query.page) || 1 }),
-  },
-  {
-    path: "/event/:id",
-    name: "EventLayout",
-    props: true,
-    component: EventLayout,
-    children: [
-      {
-        path: "",
-        name: "EventDetails",
-        component: EventDetails,
-      },
-      {
-        path: "register",
-        name: "EventRegister",
-        component: EventRegister,
-      },
-      {
-        path: "edit",
-        name: "EventEdit",
-        component: EventEdit,
-      },
-    ],
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-];
+    {
+        path: '/',
+        name: 'EventList',
+        component: EventList,
+        props: (route) => ({ page: parseInt(route.query.page) || 1 }),
+    },
+    {
+        path: '/events/:id',
+        name: 'EventLayout',
+        props: true,
+        component: EventLayout,
+        children: [
+            {
+                path: '',
+                name: 'EventDetails',
+                component: EventDetails,
+            },
+            {
+                path: 'register',
+                name: 'EventRegister',
+                component: EventRegister,
+            },
+            {
+                path: 'edit',
+                name: 'EventEdit',
+                component: EventEdit,
+            },
+        ],
+    },
+    {
+        path: '/event/:afterEvent(.*)',
+        redirect: (to) => {
+            return { path: `/events/${to.params.afterEvent}` }
+        },
+        // path: '/event/:id',
+        // redirect: () => {
+        //     return { name: 'EventDetails' }
+        // },
+        // children: [
+        //     {
+        //         path: 'register',
+        //         redirect: () => {
+        //             return { name: 'EventRegister' }
+        //         },
+        //     },
+        // ],
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-});
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+})
 
-export default router;
+export default router
