741b01db08a72aaecd825616a993270e3e710d7a
josephrebne <joey@chargeover.com>
2025-02-06 15:00:16 -0600

    figured it out

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -2,14 +2,14 @@
 import { useRouter } from 'vue-router'
 import { defineProps, inject } from 'vue'
 
-const { event } = defineProps(['event'])
+const props = defineProps(['event'])
 const router = useRouter()
 const GStore = inject('GStore')
 
 const register = () => {
     // Call to API
     // If registered then redirect to event details
-    GStore.flashMessage = 'You are successfully registered for ' + event.title
+    GStore.flashMessage = `You are successfully registered for ${props.event.title}`
 
     setTimeout(() => {
         GStore.flashMessage = ''
