b12de08a0972fee2ad0415268f11f277e897be83
josephrebne <joey@chargeover.com>
2025-02-06 14:35:03 -0600

    finished

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,86 +1,88 @@
 <script setup>
-import EventCard from "@/components/EventCard.vue";
-import EventService from "@/services/EventService.js";
-import { computed, defineProps, onMounted, ref, watch } from "vue";
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+import { computed, defineProps, onMounted, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 
-const props = defineProps(["page"]);
+const props = defineProps(['page'])
+const router = useRouter()
 
-const events = ref(null);
-const totalEvents = ref(0);
+const events = ref(null)
+const totalEvents = ref(0)
 
 const hasNextPage = computed(() => {
-	const totalPages = Math.ceil(totalEvents.value / 2);
-	return props.page < totalPages;
-});
+    const totalPages = Math.ceil(totalEvents.value / 2)
+    return props.page < totalPages
+})
 const fetchEvents = () => {
-	EventService.getEvents(2, props.page)
-		.then((response) => {
-			events.value = response.data;
-			totalEvents.value = response.headers["x-total-count"];
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-};
+    EventService.getEvents(2, props.page)
+        .then((response) => {
+            events.value = response.data
+            totalEvents.value = response.headers['x-total-count']
+        })
+        .catch(() => {
+            router.push({ name: 'NetworkError' })
+        })
+}
 
 onMounted(() => {
-	fetchEvents();
-});
+    fetchEvents()
+})
 
 watch(
-	() => props.page,
-	() => {
-		fetchEvents();
-	}
-);
+    () => props.page,
+    () => {
+        fetchEvents()
+    }
+)
 </script>
 
 <template>
-	<h1>Events for Good</h1>
-	<div class="events">
-		<EventCard v-for="event in events" :key="event.id" :event="event" />
+    <h1>Events for Good</h1>
+    <div class="events">
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
 
-		<div class="pagination">
-			<router-link
-				id="page-prev"
-				:to="{ name: 'EventList', query: { page: page - 1 } }"
-				rel="prev"
-				v-if="page != 1"
-				>&#60; Previous</router-link
-			>
+        <div class="pagination">
+            <router-link
+                id="page-prev"
+                :to="{ name: 'EventList', query: { page: page - 1 } }"
+                rel="prev"
+                v-if="page != 1"
+                >&#60; Previous</router-link
+            >
 
-			<router-link
-				id="page-next"
-				:to="{ name: 'EventList', query: { page: page + 1 } }"
-				rel="next"
-				v-if="hasNextPage"
-				>Next &#62;</router-link
-			>
-		</div>
-	</div>
+            <router-link
+                id="page-next"
+                :to="{ name: 'EventList', query: { page: page + 1 } }"
+                rel="next"
+                v-if="hasNextPage"
+                >Next &#62;</router-link
+            >
+        </div>
+    </div>
 </template>
 
 <style scoped>
 .events {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 .pagination {
-	display: flex;
-	width: 290px;
+    display: flex;
+    width: 290px;
 }
 .pagination a {
-	flex: 1;
-	text-decoration: none;
-	color: #2c3e50;
+    flex: 1;
+    text-decoration: none;
+    color: #2c3e50;
 }
 
 #page-prev {
-	text-align: left;
+    text-align: left;
 }
 
 #page-next {
-	text-align: right;
+    text-align: right;
 }
 </style>
