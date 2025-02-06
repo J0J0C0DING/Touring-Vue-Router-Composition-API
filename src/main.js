655e0049c73da45bba3469e8e4fd0f6ed4d41bf5
josephrebne <joey@chargeover.com>
2025-02-06 14:56:51 -0600

    finished (kinda, the event is weirdly not loading correctly in Register.vue 9-12

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp, reactive } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
+app.use(router)
 
-app.mount("#app");
+const GStore = reactive({ flashMessage: '' })
+app.provide('GStore', GStore)
+
+app.mount('#app')
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,19 +1,26 @@
 <script setup>
-import { useRouter } from "vue-router";
-import { defineProps } from "vue";
+import { useRouter } from 'vue-router'
+import { defineProps, inject } from 'vue'
 
-defineProps(["event"]);
-const router = useRouter();
+const { event } = defineProps(['event'])
+const router = useRouter()
+const GStore = inject('GStore')
 
 const register = () => {
-  // Call to API
-  // If registered then redirect to event details
-  router.push({
-    name: "EventDetails",
-  });
-};
+    // Call to API
+    // If registered then redirect to event details
+    GStore.flashMessage = 'You are successfully registered for ' + event.title
+
+    setTimeout(() => {
+        GStore.flashMessage = ''
+    }, 3000)
+    router.push({
+        name: 'EventDetails',
+    })
+}
 </script>
 <template>
-  <p>Register for the event here</p>
-  <button @click="register">Register Me</button>
+    <p>Register for the event here</p>
+
+    <button @click="register">Register Me</button>
 </template>
